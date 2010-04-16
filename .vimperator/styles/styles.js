cd5fd740a5f8e7db555f72db27292101e43e7562
Gigamo <gigamo@gmail.com>
2010-04-16 13:53:29 +0200

    Update vimperator configs

--- .vimperator/styles/styles.js
+++ .vimperator/styles/styles.js
@@ -1,51 +1,41 @@
-/**
- * Adds all the userstyles to vimperator
- *
- */
+var loaduserstyles = function() {
+  var userstyles = [
+    [
+      'wikipedia:minimal',
+      'en.wikipedia.org',
+      'wikipedia.css'
+    ],
 
-var loaduserstyles = function()
-{
-    /// Userstyles
-    var userstyles = [
-        [
-            "wikipedia:minimal",
-            "en.wikipedia.org",
-            "wikipedia.css",
-        ],
+    [
+      'reddit:minimal',
+      'reddit.com',
+      'reddit.css'
+    ],
 
-        [
-            "reddit:minimal",
-            "reddit.com",
-            "reddit.css",
-        ],
+    [
+      'archlinux-forums:minimal',
+      'bbs.archlinux.org',
+      'archlinux.forum.css'
+    ],
 
-        [
-            "archlinux-forums:minimal",
-            "bbs.archlinux.org",
-            "archlinux.forum.css",
-        ],
-    ];
+    [
+      'google:minimal',
+      'http://www.google.com/search*',
+      'google.css'
+    ]
+  ], styledir = io.getRuntimeDirectories('styles')[0].path;
 
-    // Get style directory
-    var styledir = io.getRuntimeDirectories("styles")[0].path;
-
-    for ([i, [name, filter, file]] in Iterator(userstyles))
-    {
-      // Remove all sheets with this filter
-      styles.removeSheet(false, name)
-
-      // Add the sheet
-      styles.addSheet(false, name, filter, 
-      File(styledir+"/"+file).read())
-    }
+  for ([i, [name, filter, file]] in Iterator(userstyles)) {
+    styles.removeSheet(false, name);
+    styles.addSheet(false, name, filter, 
+    File(styledir+'/'+file).read());
+  }
 }
 
-// Initial load
-loaduserstyles()
+loaduserstyles();
 
-// Add it as a command
 commands.add(
-    ["loaduserstyles"],
-    "Load all user styles",
-    loaduserstyles
+  ['loaduserstyles'],
+  'Load all user styles',
+  loaduserstyles
 );
